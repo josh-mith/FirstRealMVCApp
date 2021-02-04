c8a851716eb8b6951f879bd26d138de5fa0d978b
josh-mith <53532738+josh-mith@users.noreply.github.com>
2021-02-03 21:50:59 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Assignment3.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult MovieSubmission()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult MovieSubmission(MovieResponse movieResponse)
+        {
+            TempStorage.AddSubmission(movieResponse); 
+            return View("Confirmation", movieResponse);
+        }
+        public IActionResult MovieList()
+        {
+            return View(TempStorage.Submissions.Where(submission => submission.Title != "Independence Day"));
+        }
+
+        public IActionResult MyPodcasts()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/TempStorage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment3.Models
+{
+    public class TempStorage
+    {
+        private static List<MovieResponse> submissions = new List<MovieResponse>();
+
+        public static IEnumerable<MovieResponse> Submissions => submissions;
+
+        public static void AddSubmission(MovieResponse submission)
+        {
+            submissions.Add(submission);
+        }
+    }
+}
